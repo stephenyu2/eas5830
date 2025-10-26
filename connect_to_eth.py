7c2f632f60854fbcd0679d97f41edc9441382c39
stephenyu2 <SPYKER919@gmail.com>
2025-10-25 18:31:11 -0700

    2nd

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,12 +26,14 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
+	bnburl = "https://bsc-testnet.publicnode.com"
+	w3 = Web3(HTTPProvider(bnburl))
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+		# The second section requires you to inject middleware into your w3 object and
+		# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+		# and https://web3py.readthedocs.io/en/stable/web3.contract.html
+	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+	contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
 
 	return w3, contract
 
