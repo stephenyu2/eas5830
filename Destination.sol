96ca78638b99e2ea59072f28b1f87b2093c6264d
stephenyu2 <SPYKER919@gmail.com>
2025-11-12 20:04:46 -0800

    12

--- Destination.sol
+++ Destination.sol
@@ -22,34 +22,28 @@ contract Destination is AccessControl {
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		address wrapped = underlying_tokens[_underlying_token];
-		require(wrapped != address(0), "token not registered");
-		require(_recipient != address(0), "invalid recipient");
-		require(_amount > 0, "amount must be > 0");
-		BridgeToken(wrapped).mint(_recipient, _amount);
-		emit Wrap(_underlying_token, wrapped, _recipient, _amount);
-	}
+    function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
+        address wrapped = wrapped_tokens[_underlying_token];
+        require(wrapped != address(0), "token not registered");
+        BridgeToken(wrapped).mint(_recipient, _amount);
+        emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+    }
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		address underlying = wrapped_tokens[_wrapped_token];
-		require(underlying != address(0), "token not registered");
-		require(_recipient != address(0), "invalid recipient");
-		require(_amount > 0, "amount must be > 0");
-		BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-		emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
-	}
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
+        address underlying = underlying_tokens[_wrapped_token];
+        require(underlying != address(0), "token not registered");
+        BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+        emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+    }
 
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		require(_underlying_token != address(0), "invalid underlying");
-		require(underlying_tokens[_underlying_token] == address(0), "already registered");
-		BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
-		address wrapped = address(token);
-		underlying_tokens[_underlying_token] = wrapped;
-		wrapped_tokens[wrapped] = _underlying_token;
-		tokens.push(wrapped);
-		emit Creation(_underlying_token, wrapped);
-		return wrapped;
-	}
+    function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
+        BridgeToken token = new BridgeToken(_underlying_token, name, symbol, address(this));
+        address wrapped = address(token);
+        wrapped_tokens[_underlying_token] = wrapped;
+        underlying_tokens[wrapped] = _underlying_token;
+        tokens.push(wrapped);
+        emit Creation(_underlying_token, wrapped);
+        return wrapped;
+    }
 
 }
