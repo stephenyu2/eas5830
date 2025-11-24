d1beec815e50c8c2fed17fb70d61e2833f8543ea
stephenyu2 <SPYKER919@gmail.com>
2025-11-24 02:18:27 -0800

    16

--- bridge.py
+++ bridge.py
@@ -49,4 +49,62 @@ def scan_blocks(chain, contract_info="contract_info.json"):
         print( f"Invalid chain: {chain}" )
         return 0
     
-        #YOUR CODE HERE
+    contracts = get_contract_info(chain, contract_info)
+    w3 = connect_to(chain)
+
+    warden_address = Web3.to_checksum_address(contracts["warden"]["address"])
+    warden_key = contracts["warden"]["private_key"]
+
+    source_info = contracts["source"]
+    dest_info = contracts["destination"]
+
+    source_contract = connect_to("source").eth.contract(
+        address=Web3.to_checksum_address(source_info["address"]),
+        abi=source_info["abi"]
+    )
+    dest_contract = connect_to("destination").eth.contract(
+        address=Web3.to_checksum_address(dest_info["address"]),
+        abi=dest_info["abi"]
+    )
+
+    if chain == "source":
+        w3s = connect_to("source")
+        end_block = w3s.eth.get_block_number()
+        start_block = end_block - 5
+        event_filter = source_contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters={})
+        events = event_filter.get_all_entries()
+        w3d = connect_to("destination")
+        for evt in events:
+            token = evt.args["token"]
+            recipient = evt.args["recipient"]
+            amount = evt.args["amount"]
+            nonce = w3d.eth.get_transaction_count(warden_address)
+            tx = dest_contract.functions.wrap(token, recipient, amount).build_transaction({
+                "from": warden_address,
+                "nonce": nonce,
+                "gas": 500000,
+                "gasPrice": w3d.eth.gas_price
+            })
+            signed = w3d.eth.account.sign_transaction(tx, private_key=warden_key)
+            w3d.eth.send_raw_transaction(signed.rawTransaction)
+
+    if chain == "destination":
+        w3d = connect_to("destination")
+        end_block = w3d.eth.get_block_number()
+        start_block = end_block - 5
+        event_filter = dest_contract.events.Unwrap.create_filter(from_block=start_block,to_block=end_block,argument_filters={})
+        events = event_filter.get_all_entries()
+        w3s = connect_to("source")
+        for evt in events:
+            token = evt.args["underlying_token"]
+            recipient = evt.args["to"]
+            amount = evt.args["amount"]
+            nonce = w3s.eth.get_transaction_count(warden_address)
+            tx = source_contract.functions.withdraw(token, recipient, amount).build_transaction({
+                "from": warden_address,
+                "nonce": nonce,
+                "gas": 500000,
+                "gasPrice": w3s.eth.gas_price
+            })
+            signed = w3s.eth.account.sign_transaction(tx, private_key=warden_key)
+            w3s.eth.send_raw_transaction(signed.rawTransaction)
